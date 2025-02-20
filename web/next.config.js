4ffc686b444f76f6348a1d3b4da6698371c38fbe
rolodexter <jmaristela2023@gmail.com>
2025-02-20 11:54:18 +0800

    Update next.config.js

--- web/next.config.js
+++ web/next.config.js
@@ -16,15 +16,14 @@ const withMDX = require('@next/mdx')({
 const nextConfig = {
   output: 'standalone',
   async rewrites() {
-    const API_URL = process.env.NEXT_PUBLIC_API_BASE_URL || 'http://localhost:5000'
     return [
       {
         source: '/api/:path*',
-        destination: `${API_URL}/api/:path*`
+        destination: '/api/:path*'
       },
       {
         source: '/health',
-        destination: `${API_URL}/health`
+        destination: '/health'
       }
     ]
   },
