6e8105f9a18b9588ff3e35d618c6fc6497be50a3
rolodexter <jmaristela2023@gmail.com>
2025-02-20 11:49:43 +0800

    Update page.tsx

--- web/app/page.tsx
+++ web/app/page.tsx
@@ -1,7 +1,14 @@
-import { redirect } from 'next/navigation'
+'use client'
 
-const Home = async () => {
-  redirect('/signin')
+import { useEffect } from 'react'
+import { useRouter } from 'next/navigation'
+
+export default function Home() {
+  const router = useRouter()
+
+  useEffect(() => {
+    router.push('/signin')
+  }, [router])
+
+  return null
 }
-
-export default Home
