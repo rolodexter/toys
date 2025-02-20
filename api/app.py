69ed2efc0aba0a682f790e4df00b7072d0b18d4b
rolodexter <jmaristela2023@gmail.com>
2025-02-20 09:31:52 +0800

    Configure for Railway deployment

--- api/app.py
+++ api/app.py
@@ -38,4 +38,5 @@ else:
     celery = app.extensions["celery"]
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5001)
+    port = int(os.environ.get("PORT", 5001))
+    app.run(host="0.0.0.0", port=port)
